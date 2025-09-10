a25c05f87f0f7d4eae77ab318cc856f70881e6e8
Siwadol-EngKub <siwadol.arn@dome.tu.ac.th>
2025-09-11 06:27:47 +0700

    update part2

--- Part2.c
+++ Part2.c
@@ -4,7 +4,7 @@ int main() {
     float a, b, c;
 
 
-    printf("Please enter length of each side of your triangle (a b c): ");
+    printf("Please enter length of each side of your triangle : ");
     scanf("%f %f %f", &a, &b, &c);
 
     if (a <= 0 || b <= 0 || c <= 0 || 
