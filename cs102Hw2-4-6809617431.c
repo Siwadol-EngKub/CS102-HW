3eef86cf61da057f7f61eb5d811f8ab99705c31c
Siwadol-EngKub <siwadol.arn@dome.tu.ac.th>
2025-09-11 08:30:35 +0700

    Update cs102Hw2-4-6809617431.c

--- cs102Hw2-4-6809617431.c
+++ cs102Hw2-4-6809617431.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
+int main() {
+	
+	int Binteger,Linteger ;
+	
+	printf("Please enter the beginning integer :");
+	scanf("%d",&Binteger);
+	
+	printf("Please enter the last integer :");
+	scanf("%d",&Linteger);
+	
+	printf("All the numbers that can be divided by 13 are : \n");
+	
+	
+	int i ;
+	
+	for ( i =Binteger ; i <= Linteger; i++) {
+		if (i %13==0) {
+		printf("%d \n",i);
+	    }
+		
+	}
 
+	printf("Bye!!");
+	
+	
+	return 0;
+	
+	
+}
