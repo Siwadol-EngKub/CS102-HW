78f727692ba840efb335ed292d13e76f124f48f7
Siwadol-EngKub <siwadol.arn@dome.tu.ac.th>
2025-09-11 08:30:12 +0700

    Update cs102Hw2-3-6809617431.c

--- cs102Hw2-3-6809617431.c
+++ cs102Hw2-3-6809617431.c
@@ -1,1 +1,21 @@
-
+#include <stdio.h>
+int main() {
+	int secs;
+	
+	printf("Please enter the time in secondes:");
+	scanf("%d",&secs);
+	
+	if (secs<0) {
+		printf("Invalid input!!");
+	}
+	
+	int hours,minutes,seconds ;
+	hours = secs/3600 ;
+	minutes = (secs%3600)/60 ;
+	seconds = secs%60;
+	
+	printf("The time is %d hour(s), %d minute(s), and %d second(s).", hours,minutes,seconds);
+	
+	return 0;
+	
+}
